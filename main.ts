4bce0e5cf69389de6f03740608309d7976f45ef9
HarryKoch042018 <68817402+HarryKoch042018@users.noreply.github.com>
2020-07-26 17:41:21 +0200

    Aktualisiere pxt.json, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,3 +1,51 @@
-basic.forever(function () {
-	
+let zahl: number;
+basic.forever(function on_forever() {
+    if (input.buttonIsPressed(Button.A)) {
+        
+    }
+    
+    if (input.buttonIsPressed(Button.B)) {
+        
+    }
+    
 })
+for (let i = 0; i < 5; i++) {
+    zahl = Math.randomRange(0, 1)
+    if (zahl == 0) {
+        printleft()
+    } else {
+        printright()
+    }
+    
+    basic.pause(2000)
+    turn_all_off()
+    basic.pause(1000)
+}
+function turn_all_off() {
+    for (let col = 0; col < 5; col++) {
+        for (let row = 0; row < 5; row++) {
+            led.unplot(col, row)
+        }
+    }
+}
+
+function printleft() {
+    basic.showLeds(`
+    . . # . .
+    . # . . .
+    # # # # #
+    . # . . .
+    . . # . .
+    `)
+}
+
+function printright() {
+    basic.showLeds(`
+    . . # . .
+    . . . # .
+    # # # # #
+    . . . # .
+    . . # . .
+    `)
+}
+
